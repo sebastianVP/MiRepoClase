eb03c62bab3ee715540425e3da195bad869c20a9
sebastianVP <alex.valdezp22@gmail.com>
2024-12-02 21:19:47 -0500

    metodo nuevo de multiplicar

--- calculadora.py
+++ calculadora.py
@@ -5,6 +5,10 @@ def suma(a,b):
 def resta(a,b):
     return  a-b
 
+def multiplicar(a,b):
+    return  a*b
+
 print("Bienvenido a la calculadora")
 print("El resultaod de la suma 3+5 es:",suma(3,5))
 print("El resultadoa de la resta de 10-4 es:",resta(10,4))
+print("El resultado de la multiplicacion de 5x 10 es:",multiplicar(5,10))
