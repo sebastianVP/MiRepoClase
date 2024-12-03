d7002ea279e8028f4e04b7ee7580905d9c25d94e
sebastianVP <alex.valdezp22@gmail.com>
2024-12-02 20:50:16 -0500

    Se añade el metodo resta

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,10 @@
 def suma(a,b):
     return a+b
 
+
+def resta(a,b):
+    return  a-b
+
 print("Bienvenido a la calculadora")
 print("El resultaod de la suma 3+5 es:",suma(3,5))
+print("El resultadoa de la resta de 10-4 es:",resta(10,4))
